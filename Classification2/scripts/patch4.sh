0b6fa3b66e23dfdabf94bbfea28edfdc5413fd7b
effervescent-tablets-in-water <2151686126@qq.com>
2026-05-30 18:25:38 +0800

    实验5：不同patch参数实验

---
+++ Classification2/scripts/patch4.sh
@@ -0,0 +1,138 @@
+#python src/main.py \
+#    --output_dir experiments \
+#    --comment "classification from Scratch" \
+#    --name AtrialFibrillation \
+#    --records_file Classification_records.xls \
+#    --data_dir ./datasets/AtrialFibrillation \
+#    --data_class tsra \
+#    --pattern TRAIN \
+#    --val_pattern TEST \
+#    --epochs 100 \
+#    --lr 0.0005 \
+#    --patch_size 4 \
+#    --stride 2 \
+#    --optimizer AdamW \
+#    --d_model 768 \
+#    --pos_encoding learnable \
+#    --task classification \
+#    --key_metric accuracy \
+#    --gpu 4 \
+#    --seed 42 \
+#    --gpt_layers 6 \
+#    --num_mamba_layers 2
+#
+#python src/main.py \
+#    --output_dir experiments \
+#    --comment "classification from Scratch" \
+#    --name BasicMotions \
+#    --records_file Classification_records.xls \
+#    --data_dir ./datasets/BasicMotions \
+#    --data_class tsra \
+#    --pattern TRAIN \
+#    --val_pattern TEST \
+#    --epochs 100 \
+#    --lr 0.0005 \
+#    --patch_size 4 \
+#    --stride 2 \
+#    --optimizer AdamW \
+#    --d_model 768 \
+#    --pos_encoding learnable \
+#    --task classification \
+#    --key_metric accuracy \
+#    --gpu 4 \
+#    --seed 42 \
+#    --gpt_layers 6 \
+#    --num_mamba_layers 2
+#
+#python src/main.py \
+#    --output_dir experiments \
+#    --comment "classification from Scratch" \
+#    --name BasicMotions \
+#    --records_file Classification_records.xls \
+#    --data_dir ./datasets/BasicMotions \
+#    --data_class tsra \
+#    --pattern TRAIN \
+#    --val_pattern TEST \
+#    --epochs 100 \
+#    --lr 0.0005 \
+#    --patch_size 4 \
+#    --stride 2 \
+#    --optimizer AdamW \
+#    --d_model 768 \
+#    --pos_encoding learnable \
+#    --task classification \
+#    --key_metric accuracy \
+#    --gpu 4 \
+#    --seed 42 \
+#    --gpt_layers 6 \
+#    --num_mamba_layers 2
+#
+#python src/main.py \
+#    --output_dir experiments \
+#    --comment "classification from Scratch" \
+#    --name CharacterTrajectories \
+#    --records_file Classification_records.xls \
+#    --data_dir ./datasets/CharacterTrajectories \
+#    --data_class tsra \
+#    --pattern TRAIN \
+#    --val_pattern TEST \
+#    --epochs 100 \
+#    --lr 0.0005 \
+#    --patch_size 4 \
+#    --stride 2 \
+#    --optimizer AdamW \
+#    --d_model 768 \
+#    --pos_encoding learnable \
+#    --task classification \
+#    --key_metric accuracy \
+#    --gpu 4 \
+#    --seed 42 \
+#    --gpt_layers 6 \
+#    --num_mamba_layers 2
+
+python src/main.py \
+    --output_dir experiments \
+    --comment "classification from Scratch" \
+    --name Epilepsy \
+    --records_file Classification_records.xls \
+    --data_dir ./datasets/Epilepsy \
+    --data_class tsra \
+    --pattern TRAIN \
+    --val_pattern TEST \
+    --epochs 100 \
+    --lr 0.0005 \
+    --patch_size 4 \
+    --stride 2 \
+    --optimizer AdamW \
+    --d_model 768 \
+    --pos_encoding learnable \
+    --task classification \
+    --key_metric accuracy \
+    --gpu 4 \
+    --seed 42 \
+    --gpt_layers 6 \
+    --num_mamba_layers 2
+
+python src/main.py \
+    --output_dir experiments \
+    --comment "classification from Scratch" \
+    --name ERing \
+    --records_file Classification_records.xls \
+    --data_dir ./datasets/ERing \
+    --data_class tsra \
+    --pattern TRAIN \
+    --val_pattern TEST \
+    --epochs 100 \
+    --lr 0.0005 \
+    --patch_size 4 \
+    --stride 2 \
+    --optimizer AdamW \
+    --d_model 768 \
+    --pos_encoding learnable \
+    --task classification \
+    --key_metric accuracy \
+    --gpu 4 \
+    --seed 42 \
+    --gpt_layers 6 \
+    --num_mamba_layers 2
+
